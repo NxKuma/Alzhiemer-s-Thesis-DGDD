0f071f529e992668d554e959b244e308b4217a23
Kuma <55661040+NxKuma@users.noreply.github.com>
2025-11-21 05:21:31 +0800

    Implemented Models to NPC
    - NPC Resource Created
    - NPC Models are implemented to the Prefab
    - BUG: Cannot interact with NPC

---
+++ Alzheimer's Disease/Assets/NPCScript.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+    [SerializeField] private NPC _npcData;
+    private GameObject _nPCModel;
+
+    void Awake()
+    {
+        _nPCModel = this.transform.GetChild(0).gameObject;
+        GameObject npcMesh = Instantiate(_npcData.GetNPCPrefab(), this.transform);
+        //Make sure the Model is facing forward
+        if(_npcData.GetNPCName().Contains("Wife")) npcMesh.transform.rotation = Quaternion.Euler(0,45,0);
+        else npcMesh.transform.rotation = Quaternion.Euler(0,90,0);
+        
+        if(_npcData.GetNPCName().Contains("Daughter")) 
+        {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, 1.0f, this.transform.localPosition.z);
+            npcMesh.transform.localScale = Vector3.one * _npcData.GetNPCSize();  
+            npcMesh.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material = _npcData.GetNPCMaterial();   
+
+        }
+        else {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, 1.0f, this.transform.localPosition.z);
+            npcMesh.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material = _npcData.GetNPCMaterial();   
+        
+        }
+
+        while (_nPCModel.transform.childCount > 0)
+        {
+            Transform child = _nPCModel.transform.GetChild(0);
+            child.transform.localPosition = new Vector3(child.transform.localPosition.x, child.transform.localPosition.y + 1f, child.transform.localPosition.z);
+            if(child.name.Contains("Face"))
+            {
+                MeshRenderer rend = child.GetComponent<MeshRenderer>();
+                rend.enabled = false;
+            }else{  
+                child.transform.localScale = Vector3.one * 0.35f;
+                child.transform.localPosition = new Vector3(0f, 1.68f, 0f);
+            }
+
+            // Preserve the child's world transform (position/rotation/scale) when reparenting
+            // so it appears in the same place after being moved under `npcMesh`.
+            child.SetParent(npcMesh.transform, worldPositionStays: true);
+            
+        }
+        //Make the capsule invisible
+        _nPCModel.SetActive(false);
+        //Make sure no camera will be spawned
+        foreach(Transform child in npcMesh.transform)
+        {
+            if (child.GetComponent<Camera>() != null)
+                Destroy(child.gameObject);
+        }
+
+        
+
+
+        // MeshFilter mf = _nPCModel.GetComponent<MeshFilter>();
+        // mf.mesh = _npcData.GetNPCMesh();
+        // Renderer rend = _nPCModel.GetComponent<Renderer>();
+        // rend.material = _npcData.GetNPCMaterial();
+        // _nPCModel.transform.localScale = Vector3.one * _npcData.GetNPCSize();
+    }
+
+    
+
+    void Start()
+    {
+        
+    }
+
+
+}
---
+++ Alzheimer's Disease/Assets/Scripts/Dialogue/NPC.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC", menuName = "ScriptableObjects/NPC", order = 0)]
+public class NPC : ScriptableObject
+{
+    [Header("NPC Data")]
+    [SerializeField]
+    private string _npcName;
+    
+    // Area for Other NPC details
+
+    [Header("NPC Mesh Data")]
+    [SerializeField] private Mesh _npcMesh;
+    [SerializeField] private Material _npcMaterial;
+    [SerializeField] private Texture _npcFace;
+    [SerializeField] private float _npcSize;
+    [SerializeField] private GameObject _npcPrefab;
+    //Getters
+    public string GetNPCName() => _npcName; 
+    public Mesh GetNPCMesh() => _npcMesh; 
+    public Material GetNPCMaterial() => _npcMaterial; 
+    public Texture GetNPCFace() => _npcFace; 
+    public float GetNPCSize() => _npcSize;
+    public GameObject GetNPCPrefab() => _npcPrefab;
+}
