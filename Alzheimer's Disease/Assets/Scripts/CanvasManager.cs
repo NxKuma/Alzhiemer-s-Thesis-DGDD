403377eb46cb7bd1365579178a8d285d317385f2
Kuma <55661040+NxKuma@users.noreply.github.com>
2026-01-14 17:33:54 +0800

    Added New Meshes
    - Added Items as Scriptable Objects
    - Prepped Rooms

--- Alzheimer's Disease/Assets/Scripts/CanvasManager.cs
+++ Alzheimer's Disease/Assets/Scripts/CanvasManager.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 
 public class CanvasManager : MonoBehaviour
 {
-    [SerializeField] private Canvas[] _canvasList;
+    [SerializeField] private Canvas[] _majorCanvasList;
+    [SerializeField] private Canvas[] _interactCanvasList;
     private static CanvasManager _instance;
     private Canvas _activeCanvas;
     private bool _isDialogueActive = false;
 
     void Awake()
     {
+        _instance = this;
+        
         if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
             return;
         }
 
-        _instance = this;
         DontDestroyOnLoad(this.gameObject);
     }
 }
